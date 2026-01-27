9fb6d64775a1c68f8de5d4c639b7ecb61036d4c8
Nehanth Godishala <nehanthgodishala04@gmail.com>
2026-01-27 22:04:49 +0530

    hope!

--- backend-api/src/api.js
+++ backend-api/src/api.js
@@ -14,12 +14,19 @@ const allowedOrigins = [
   "https://collab-edit-nehanthgs-projects.vercel.app",
 ];
 
-app.use(cors({
-  origin: allowedOrigins,
-  methods: ["GET", "POST"],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://collab-edit-mu.vercel.app",
+    ],
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
+app.options("*", cors());
 app.use(express.json({ limit: "200kb" }));
 
 app.use("/auth", authRoutes);
